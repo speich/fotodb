83f6976f748739e7ac7944763639fc465a916fab
speich <info@speich.net>
2017-10-03 13:18:01 +0200

    move loading and saving user preferences into own class

---
+++ classes/PhotoDatabase/Preferences.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace PhotoDatabase;
+
+
+use PDO;
+use PDOException;
+
+/**
+ * Class Preferences
+ * Store user preferences in a SQLite database
+ * @package PhotoDatabase
+ */
+class Preferences
+{
+    private $db;
+    private $dbName = 'user.sqlite';
+    private $dbPath = '/../../dbprivate/dbfiles/';
+
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO('sqlite:' . __DIR__ . $this->dbPath . $this->dbName);
+        } catch (PDOException $Error) {
+            echo $Error->getMessage();
+        }
+    }
+
+    /**
+     * Store user preference.
+     * @param string $Name preference
+     * @param string $Value value
+     * @param integer $UserId user id
+     * @return bool
+     */
+    public function save($Name, $Value, $UserId)
+    {
+
+        // get setting id
+        $Sql = "SELECT Id FROM Settings WHERE Name = :Name";
+        $Stmt = $this->db->prepare($Sql);
+        $Stmt->bindParam(':Name', $Name);
+        $Stmt->execute();
+        $Row = $Stmt->fetch(PDO::FETCH_ASSOC);
+        $SettingId = $Row['Id'];
+        // check if this setting was already set once if not insert it otherwise update
+        $Sql = "SELECT Id FROM Prefs WHERE SettingId = :SettingId AND UserId = :UserId";
+        $Stmt = $this->db->prepare($Sql);
+        $Stmt->bindParam(':SettingId', $SettingId);
+        $Stmt->bindParam(':UserId', $UserId);
+        $Stmt->execute();
+        $Row = $Stmt->fetch(PDO::FETCH_ASSOC);
+        if (!is_null($Row['Id'])) {
+            $Sql = "UPDATE Prefs SET Value = :Value WHERE SettingId = :SettingId AND UserId = :UserId";
+        } else {
+            $Sql = "INSERT INTO Prefs (SettingId, UserId, Value) VALUES (:SettingId, :UserId, :Value)";
+        }
+        $Stmt = $this->db->prepare($Sql);
+        $Stmt->bindParam(':SettingId', $SettingId);
+        $Stmt->bindParam(':UserId', $UserId);
+        $Stmt->bindParam(':Value', $Value);
+
+        return $Stmt->execute();
+    }
+
+    /**
+     * Load a user preference.
+     * @return
+     * @param string $Name preference
+     * @param integer $UserId
+     */
+    public function load($Name, $UserId)
+    {
+        try {
+            $this->db = new PDO('sqlite:' . __DIR__ . '/../dbprivate/dbfiles/' . $this->dbName);
+        } catch (PDOException $Error) {
+            echo $Error->getMessage();
+        }
+        // get setting id
+        $Sql = "SELECT Value FROM Prefs WHERE SettingId = (SELECT Id FROM Settings WHERE Name = :Name) AND UserId = :UserId";
+        $Stmt = $this->db->prepare($Sql);
+        $Stmt->bindParam(':Name', $Name);
+        $Stmt->bindParam(':UserId', $UserId);
+        $Stmt->execute();
+        $Row = $Stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $Row['Value'];
+    }
+}
--- dbprivate/library/explorer.php
+++ dbprivate/library/explorer.php
@@ -1,29 +1,34 @@
 <?php
+
+use PhotoDatabase\Preferences;
+
 include __DIR__.'/../../classes/class_website.php';
-include __DIR__.'/../../classes/class_fotodb.php';
-include __DIR__.'/../../classes/class_explorer.php';
+include __DIR__ . '/../../classes/PhotoDatabase/class_fotodb.php';
+include __DIR__ . '/../../classes/FileFileExplorer.php';
+require_once __DIR__.'/../../classes/PhotoDatabase/Preferences.php';
 error_reporting(E_ERROR);
 
 // note: all paths should end with a slash
-
-$Fs = new Explorer();
-$Fs->Connect();
-$Fs->SetTopDir($Fs->GetWebRoot().'dbprivate/images/');
+$prefs = new Preferences();
+$db = new FotoDb('private');
+$db->Connect();
+$fs = new FileExplorer($db);
+$fs->SetTopDir($fs->GetWebRoot().'dbprivate/images/');
 
 if (isset($_GET['Type'])) {
 	if (isset($_GET['Dir'])) {
 		$Path = preg_replace("/[\"'<>;:]/", "", $_GET['Dir']);
 	}
 	else {
-		$LastDir = $Fs->LoadPref('Explorer.LastDir', 1);
+		$LastDir = $prefs->load('Explorer.LastDir', 1);
 		if (is_null($LastDir)) {
-			$Path = $Fs->GetTopDir();
+			$Path = $fs->GetTopDir();
 		}
 		else {
 			$Path = $LastDir;
 		}
 	}
-	$arrFile = $Fs->ReadDirWeb($Path);
-	$Fs->SavePref('Explorer.LastDir', $Path, 1);
-	$Fs->Render($arrFile, $_GET['Type'], isset($_GET['Filter']) ? $_GET['Filter'] : false);
+	$arrFile = $fs->ReadDirWeb($Path);
+    $prefs->save('Explorer.LastDir', $Path, 1);
+	$fs->Render($arrFile, $_GET['Type'], isset($_GET['Filter']) ? $_GET['Filter'] : false);
 }
