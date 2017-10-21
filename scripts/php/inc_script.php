b6e562f74e98b55b529f74e01c87d178b0f2b820
Simon <simon.speich@gmail.com>
2017-10-21 10:51:16 +0200

    set namespace before autoloading

--- scripts/php/inc_script.php
+++ scripts/php/inc_script.php
@@ -1,12 +1,14 @@
 <?php
-require_once __DIR__.'/../library/vendor/autoload.php';
 
 use PhotoDatabase\Database\Database;
 use WebsiteTemplate\Website;
 
+
 session_start();
 date_default_timezone_set('Europe/Zurich');
 error_reporting(E_ERROR);
+
+require_once __DIR__.'/../../library/vendor/autoload.php';
 
 $db = new Database();
 $web = new Website();
