4c1e628255281e1de65ade3c4ee31215c269caca
Simon <simon.speich@gmail.com>
2019-01-06 16:32:08 +0100

    updated paths to use exiftool installed via composer

--- cgi-bin/test.php
+++ cgi-bin/test.php
@@ -4,20 +4,20 @@ ini_set('xdebug.var_display_max_depth', -1);
 ini_set('xdebug.var_display_max_children', -1);
 ini_set('xdebug.var_display_max_data', -1);
 
-$ExifToolVersion = '10.68';
-$ExifToolPath =
+$exifToolPath = '../library/vendor/philharvey/exiftool/exiftool';
+$exifToolParams = '-G';
 
 // Read EXIF from jpg
 $img = 'test.jpg';
-exec('Image-ExifTool-'.$ExifToolVersion.'/exiftool -G '.$img, $data);
+exec($exifToolPath.' '.$exifToolParams.' '.$img, $data);
 var_dump(array_unique($data));
 
 // Read EXIF from NEF
 $img = 'test.NEF';
-exec('Image-ExifTool-'.$ExifToolVersion.'/exiftool -G '.$img, $data);
+exec($exifToolPath.' '.$exifToolParams.' '.$img, $data);
 
 // Read XMP
 $img = 'test.xmp';
-exec('Image-ExifTool-'.$ExifToolVersion.'/exiftool -G '.$img, $data);
+exec($exifToolPath.' '.$exifToolParams.' '.$img, $data);
 sort($data);
 var_dump(array_unique($data));
