a1b5690a8ac80e42d493dc4dbd56d92e90140eb8
Simon <simon.speich@gmail.com>
2019-06-29 15:03:39 +0200

    - break code into smaller pieces - use exceptions

--- website/scripts/php/class/Database/Exporter.php
+++ website/scripts/php/class/Database/Exporter.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace PhotoDatabase\Database;
 
 use PDO;
 use PhotoDatabase\Thumbnail;
+use RuntimeException;
 
 
 /**
  * Class Exporter
  */
-class Exporter extends Database {
+class Exporter extends Database
+{
 
     /** @var string */
     private $pathTargetDb;
@@ -19,135 +22,161 @@ class Exporter extends Database {
     /**
      * @param \stdClass $config
      */
-	public function __construct($config) {
-		parent::__construct($config);
-		$this->pathTargetDb = $config->path->targetDatabase;
-		$this->pathTargetImages = $config->path->targetImages;
-	}
+    public function __construct($config)
+    {
+        parent::__construct($config);
+        $this->pathTargetDb = $config->paths->targetDatabase;
+        $this->pathTargetImages = $config->paths->targetImages;
+    }
 
     /**
      * Export database and images marked as public.
      * Creates a thumbnail from each exported image. The parameter mode allows either to recreate all records.
      * Warning: If the destination database file already exists, it will be overwritten.
      */
-	public function publish(): void
+    public function publish(): void
     {
-		// TODO: use x-mixed-replace with json messaging for progress feedback
-		// TODO: check permissions for exporting
-		set_time_limit(0);
-        $thumbnail = new Thumbnail();
-
-		$sourceDb = $this->connect();
+        set_time_limit(0);
+        $sourceDb = $this->connect();
         $targetDb = $this->copyAndClean();
+        $arrData = $this->getRecords();
 
-		// Select all records which will be used to copy/delete images depending on their public status.
-		// IMPORTANT: Do not limit sql to only public ones by using destDb, because then you would miss deleting
-		// thumbnails that changed state from public in previous export to private in this export
-		// We purposely do not use WHERE IN, instead we update records in a loop one at a time after creation of
-		// the thumbnail (since we don't use a transaction because journaling mode is off for speed)
-		$sql = "SELECT Id, ImgFolder, ImgFolder||'/'||ImgName Img, Public FROM Images
-			WHERE (LastChange > DatePublished OR DatePublished IS NULL)";
-		$stmt = $sourceDb->prepare($sql);
-		$stmt->execute();
-		$arrData = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-		// source db
-		$sql = 'UPDATE Images SET DatePublished = :Time WHERE Id = :ImgId';
-		$stmt = $sourceDb->prepare($sql);
-		$stmt->bindParam(':Time', $time);
-		$stmt->bindParam(':ImgId', $imgId);
-		// dest db
-		$sql = 'UPDATE Images SET DatePublished = :Time WHERE Id = :ImgId';
-		$stmt1 = $targetDb->prepare($sql);
-		$stmt1->bindParam(':Time', $time);
-		$stmt1->bindParam(':ImgId', $imgId);
-		// remove location information where necessary
-		// TODO: also remove location from exif
-		$sql = "UPDATE Images SET ImgLat = NULL, ImgLng = NULL WHERE Id = :ImgId AND ShowLoc IS NULL OR ShowLoc = '0'";
-		$stmt2 = $targetDb->prepare($sql);
-		$stmt2->bindParam(':ImgId', $imgId);
-
-		foreach ($arrData as $row) {
-			$srcImg = __DIR__.'/../../../../'.$row['Img'];
-			$destImg = $this->pathTargetImages.'/'.$row['Img'];
-			// copy image
-			if ($row['Public'] === '1') {
-				$dir = $this->pathTargetImages.'/'.$row['ImgFolder'];
-				if (!is_dir($dir)) {
-					$succ = mkdir($dir, 0777, true);
-					if (!$succ) {
-						exit('making of dir '.$dir.' failed.');
-					}
-					$succ = mkdir(str_replace('images/', 'images/thumbs/', $dir), 0777, true);
-					if (!$succ) {
-						exit('making of dir '.$dir.' failed.');
-					}
-				}
-				$succ = copy($srcImg, $destImg);
-				if (!$succ) {
-					exit('copying of img '.$srcImg.' failed.');
-				}
-				// create thumbnail
-                $destPath = str_replace('images/', 'images/thumbs/', $destImg);
-				$succ = $thumbnail->create($destImg, $destPath,180);
-				// update rec where img is exported so in case of a resume we do not have to re-export it
-				if ($succ) {
-					$time = time(); // bound to sql query above
-					$imgId = $row['Id']; // bound to sql query above
-					$stmt->execute();
-					$stmt1->execute();
-					$stmt2->execute();
-					echo "exported $destImg {$row['Id']}<br>";
-				}
-			}
-			// delete previously copied images that are no longer public
-			else {
-				if (is_file($destImg)) {
-					unlink($destImg);
-					echo "deleted $destImg<br>";
-				}
-				$stmt2->execute();
-			}
-		}
-	}
+        // source db
+        $sql = 'UPDATE Images SET DatePublished = :Time WHERE Id = :ImgId';
+        $stmtDateSrc = $sourceDb->prepare($sql);
+        $stmtDateSrc->bindParam(':Time', $time);
+        $stmtDateSrc->bindParam(':ImgId', $imgId);
+        // dest db
+        $sql = 'UPDATE Images SET DatePublished = :Time WHERE Id = :ImgId';
+        $stmtDateDest = $targetDb->prepare($sql);
+        $stmtDateDest->bindParam(':Time', $time);
+        $stmtDateDest->bindParam(':ImgId', $imgId);
+        // remove location information where necessary
+        // TODO: also remove location from exif
+        $sql = "UPDATE Images SET ImgLat = NULL, ImgLng = NULL WHERE Id = :ImgId AND ShowLoc IS NULL OR ShowLoc = '0'";
+        $stmtLocation = $targetDb->prepare($sql);
+        $stmtLocation->bindParam(':ImgId', $imgId);
+        foreach ($arrData as $row) {
+            $destImg = $this->pathTargetImages.'/'.$row['Img'];
+            // copy image
+            if ($row['Public'] === '1') {
+                $dir = $this->pathTargetImages.'/'.$row['ImgFolder'];
+                $this->createImgDirectories($dir);
+                $srcImg = __DIR__.'/../../../../dbprivate/images/'.$row['Img'];
+                $this->copyImages($srcImg, $destImg);
+                // update DatePublished of exported image so it will not be re-exported until LastChange is updated
+                $time = time(); // variable is bound to sql query above
+                $imgId = $row['Id']; // variable is bound to sql query above
+                $stmtDateSrc->execute();
+                $stmtDateDest->execute();
+                echo "exported $destImg {$row['Id']}<br>";
+            }
+            // delete previously copied images that are no longer public
+            else {
+                $this->deleteImage($destImg);
+                echo "deleted $destImg {$row['Id']}<br>";
+            }
+            $stmtLocation->execute();
+        }
+    }
 
     /**
      * Copy database file to target database file.
      * Instead of looping through all tables and records to find records that have changed or were added since
      * the last publishing. We just copy the whole db file and then remove the private records of the images table before.
-     * We ignore deleting of keywords, etc. because they don't really matter and to keep it simple
+     * We ignore deleting of keywords, etc. because they don't really matter and to keep it simple.
      * Previous target database file will be overwritten.
      * @return PDO target database
      */
     public function copyAndClean(): PDO
     {
-        $source = $this->GetPath('Db').$this->getDbName();
-        echo 'source db: '.print_r(file_exists($source), true)."<br>";
-        echo 'dest db: '.print_r(file_exists($this->pathTargetDb), true)."<br>";
+        $source = $this->GetPath('Db');
         if (copy($source, $this->pathTargetDb)) {
-            echo 'db copy successful<br>';
-
             // For speed reasons we turn journaling off and increase cache size. As long as we import all records at once,
             // we do not need a rollback. We just start over in case of a crash. This is only possible for the destination
             // db, since the file might get corrupted.
             // default = 2000 pages, 1 page = 1kb;
             $targetDb = new PDO('sqlite:'.$this->pathTargetDb);
+            $targetDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = 'PRAGMA journal_mode = OFF; PRAGMA cache_size = 10000;';
             $targetDb->exec($sql);
 
             // Records with Public == 0 have always to be deleted independent of DatePublished, because they are copied with the database !
             $sql = "DELETE FROM Images WHERE Public = '0'";
-            if ($targetDb->exec($sql) === false) {
-                exit('deleting records failed');
-            }
-        } else {
-            exit('publishing failed');
+            $targetDb->exec($sql);
+
+            return $targetDb;
         }
 
-        return $targetDb;
+        $err = error_get_last();
+        throw new RuntimeException($err['message'], $err['code']);
     }
 
-    public function createSearch() {
+    /**
+     * Query records to process in export.
+     * @return mixed
+     */
+    private function getRecords() {
+        // Select all records from source database which will be used to copy/delete images depending on their public status.
+        // IMPORTANT: Do not limit sql to only public ones by using destDb, because then you would miss deleting
+        // thumbnails that changed state from public in previous export to private in this export
+        // We purposely do not use WHERE IN, instead we update records in a loop one at a time after creation of
+        // the thumbnail (since we don't use a transaction because journaling mode is off for speed)
+        // TODO: queries to many records, over and over again, e.g. where Public = 0 or DatePublished = NULL
+        $sql = "SELECT Id, ImgFolder, ImgFolder||'/'||ImgName Img, Public FROM Images
+			WHERE (LastChange > DatePublished OR DatePublished IS NULL)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Delete an image from the filesystem.
+     * @param $img
+     */
+    public function deleteImage($img): void
+    {
+        if ((is_file($img) === true) && unlink($img) === false) {
+            throw new RuntimeException('could not delete image: '.$img);
+        }
+    }
+
+    /**
+     * Creates a directory and a thumbnail directory for the image if it does not exist.
+     * @param string $dir directory path
+     */
+    private function createImgDirectories($dir): void
+    {
+        if (!is_dir($dir)) {
+            if (mkdir($dir, 0777, true) === false) {
+                throw new RuntimeException('creating directory '.$dir.' failed.');
+            }
+            $dirThumbnails = str_replace('/images/', '/images/thumbs/', $dir);
+            if (mkdir($dirThumbnails, 0777, true) === false) {
+                throw new RuntimeException('creating thumbnails directory '.$dir.' failed.');
+            }
+        }
+    }
+
+    /**
+     * Copy image and create thumbnail.
+     * @param string $srcImg image path
+     * @param string $destImg image path
+     */
+    private function copyImages($srcImg, $destImg): void
+    {
+        if (copy($srcImg, $destImg)) {
+            $thumbnail = new Thumbnail();
+            $destPath = str_replace('/images/', '/images/thumbs/', $destImg);
+            $thumbnail->create($destImg, $destPath, 180);
+        } else {
+            throw new RuntimeException('copying of image '.$srcImg.' failed.');
+        }
+    }
+
+    public function createSearch()
+    {
 
     }
 
--- website/scripts/php/controller/dbfunctions.php
+++ website/scripts/php/controller/dbfunctions.php
@@ -2,15 +2,27 @@
 // TODO: this code should only be available to authenticated users (->PHP)
 // specially the delete function!!!!
 // TODO: check all input before storing in db
+use PhotoDatabase\Database\Database;
 use PhotoDatabase\Database\Exporter;
+use PhotoDatabase\Database\SearchKeywords;
+use WebsiteTemplate\Controller;
+use WebsiteTemplate\Error;
+use WebsiteTemplate\Header;
 
-require_once '../inc_script.php';
 
+require_once __DIR__.'/../inc_script.php';
+$header = new Header();
+$header->setContentType('json');
+$err = new Error();
+$ctrl = new Controller($header, $err);
+$data = $ctrl->getDataAsObject();
+$data = $data ?? new stdClass();
+$resources = $ctrl->getResource();
+$method = $ctrl->getMethod();
+$response = null;
 
-
-$fnc = $_POST['Fnc'] ?? $_GET['Fnc'] ?? null;
-if ($fnc) {
-    switch ($fnc) {
+if (property_exists($data, 'Fnc')) {
+    switch ($data->Fnc) {
         case 'Insert':
             // insert new image and return new id
             $db->Insert($_POST['Img']);
@@ -42,77 +54,84 @@ if ($fnc) {
             // load specific form data, e.g. locations in a certain country or a scientific name
             $db->LoadData();
             break;
-    }
-}
 
-if (isset($_GET['Fnc']) && $_GET['Fnc'] === 'Publish') {
-        $exporter = new Exporter($config);
-        $exporter->connect();
-        $exporter->publish();
+        case 'Publish':
+            echo 'exporting database...<br>';
+            try {
+                $exporter = new Exporter($config);
+                $db = $exporter->connect();
+                $exporter->publish();
+                echo 'db copy successful<br>';
+            }
+            catch (RuntimeException $exception) {
+                echo 'could not export database:<br>';
+                echo $exception->getMessage();
+            }
 
-        // create/update search indexes in the target database
-        /*
-            $indexer = new \PhotoDatabase\Database\SearchKeywords($destDb);
+            // create/update search indexes in the target database
+            //$config->paths->database = '/var/www/html/fotodb/website/dbprivate/dbfiles/FotoDb.sqlite';
+            //$db = new Database($config);
+            //$db = $db->connect();
+            $indexer = new SearchKeywords($db);
             //$destDb->sqliteCreateFunction('RANK', 'rank');
             $succ = $indexer->create();
             var_dump($indexer->db->errorInfo());
             $succ = $indexer->populate();
-            $result = $indexer->search('krah');
+            $result = $indexer->search('wäld');
             var_dump($result);
-
-        $destDb = new PDO('sqlite:'.$config->paths->targetDatabase);
-            $indexer = new \PhotoDatabase\Database\SearchImages($destDb);
-            //$succ = $indexer->create();
-            //$succ = $indexer->populate();
-            $result = $indexer->search('jap');
-            var_dump($result);
-          */
+            /*
+                    $destDb = new PDO('sqlite:'.$config->paths->targetDatabase);
+                        $indexer = new \PhotoDatabase\Database\SearchImages($destDb);
+                        //$succ = $indexer->create();
+                        //$succ = $indexer->populate();
+                        $result = $indexer->search('jap');
+                        var_dump($result);
+                      */
+            break;
+    }
 }
 
 function rank($aMatchInfo)
-    {
-        $iSize = 4;
-        $iPhrase = (int) 0;                 // Current phrase //
-        $score = (double)0.0;               // Value to return //
-        /* Check that the number of arguments passed to this function is correct.
-        ** If not, jump to wrong_number_args. Set aMatchinfo to point to the array
-        ** of unsigned integer values returned by FTS function matchinfo. Set
-        ** nPhrase to contain the number of reportable phrases in the users full-text
-        ** query, and nCol to the number of columns in the table.
+{
+    $iSize = 4;
+    $iPhrase = (int)0;                 // Current phrase //
+    $score = (double)0.0;               // Value to return //
+    /* Check that the number of arguments passed to this function is correct.
+    ** If not, jump to wrong_number_args. Set aMatchinfo to point to the array
+    ** of unsigned integer values returned by FTS function matchinfo. Set
+    ** nPhrase to contain the number of reportable phrases in the users full-text
+    ** query, and nCol to the number of columns in the table.
+    */
+    $aMatchInfo = (string)func_get_arg(0);
+    //$str = ord($aMatchInfo);
+    //$int = $this->toInt($aMatchInfo);
+    $nPhrase = ord(substr($aMatchInfo, 0, $iSize));
+    $nCol = ord(substr($aMatchInfo, $iSize, $iSize));
+    if (func_num_args() > (1 + $nCol)) {
+        throw new Exception('Invalid number of arguments : '.$nCol);
+    }
+    // Iterate through each phrase in the users query. //
+    for ($iPhrase = 0; $iPhrase < $nPhrase; $iPhrase++) {
+        $iCol = (int)0; // Current column //
+        /* Now iterate through each column in the users query. For each column,
+        ** increment the relevancy score by:
+        **
+        **   (<hit count> / <global hit count>) * <column weight>
+        **
+        ** aPhraseinfo[] points to the start of the data for phrase iPhrase. So
+        ** the hit count and global hit counts for each column are found in
+        ** aPhraseinfo[iCol*3] and aPhraseinfo[iCol*3+1], respectively.
         */
-        $aMatchInfo = (string) func_get_arg(0);
-        //$str = ord($aMatchInfo);
-        //$int = $this->toInt($aMatchInfo);
-        $nPhrase = ord(substr($aMatchInfo, 0, $iSize));
-        $nCol = ord(substr($aMatchInfo, $iSize, $iSize));
-        if (func_num_args() > (1 + $nCol))
-        {
-            throw new Exception("Invalid number of arguments : ".$nCol);
-        }
-        // Iterate through each phrase in the users query. //
-        for ($iPhrase = 0; $iPhrase < $nPhrase; $iPhrase++)
-        {
-            $iCol = (int) 0; // Current column //
-            /* Now iterate through each column in the users query. For each column,
-            ** increment the relevancy score by:
-            **
-            **   (<hit count> / <global hit count>) * <column weight>
-            **
-            ** aPhraseinfo[] points to the start of the data for phrase iPhrase. So
-            ** the hit count and global hit counts for each column are found in
-            ** aPhraseinfo[iCol*3] and aPhraseinfo[iCol*3+1], respectively.
-            */
-            $aPhraseinfo = substr($aMatchInfo, (2 + $iPhrase * $nCol * 3) * $iSize);
-            for ($iCol = 0; $iCol < $nCol; $iCol++)
-            {
-                $nHitCount = ord(substr($aPhraseinfo, 3 * $iCol * $iSize, $iSize));
-                $nGlobalHitCount = ord(substr($aPhraseinfo, (3 * $iCol + 1) * $iSize, $iSize));
-                $weight = ($iCol < func_num_args() - 1) ? (double) func_get_arg($iCol + 1) : 0;
-                if ($nHitCount > 0)
-                {
-                    $score += ((double)$nHitCount / (double)$nGlobalHitCount) * $weight;
-                }
+        $aPhraseinfo = substr($aMatchInfo, (2 + $iPhrase * $nCol * 3) * $iSize);
+        for ($iCol = 0; $iCol < $nCol; $iCol++) {
+            $nHitCount = ord(substr($aPhraseinfo, 3 * $iCol * $iSize, $iSize));
+            $nGlobalHitCount = ord(substr($aPhraseinfo, (3 * $iCol + 1) * $iSize, $iSize));
+            $weight = ($iCol < func_num_args() - 1) ? (double)func_get_arg($iCol + 1) : 0;
+            if ($nHitCount > 0) {
+                $score += ((double)$nHitCount / (double)$nGlobalHitCount) * $weight;
             }
         }
-        return $score;
     }
+
+    return $score;
+}
