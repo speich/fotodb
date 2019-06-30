f0ebc2505e74b9fc96fde7291a848479dfb06735
Simon <simon.speich@gmail.com>
2019-06-30 16:27:11 +0200

    do not start session initially

--- website/scripts/php/inc_script.php
+++ website/scripts/php/inc_script.php
@@ -4,16 +4,14 @@ use PhotoDatabase\Database\Database;
 use WebsiteTemplate\Website;
 
 
-session_start();
 date_default_timezone_set('Europe/Zurich');
 error_reporting(E_ERROR);
 
 require_once __DIR__.'/../../library/vendor/autoload.php';
 
 $file = file_get_contents(__DIR__.'/config.json');
-$config = json_decode($file);
+$config = json_decode($file, false);
 $db = new Database($config);
 $db->connect();
 
 $web = new Website();
-
