5db7f509ddda83ba769cac5f603d271d8d5cc43a
Simon <simon.speich@gmail.com>
2020-06-14 15:15:36 +0200

    Removing website/library/vendor from git history

---
+++ website/scripts/php/class/Search/ImagesSearch.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace PhotoDatabase\Search;
+
+use PDO;
+
+
+/**
+ * Class SearchKeywords
+ * Provides a list of (key)words found in all relevant columns of all relevant tables to display
+ * @package PhotoDatabase\Database
+ */
+class ImagesSearch
+{
+    /** @var PDO */
+    public $db;
+
+    /**
+     * Keywords constructor.
+     * @param PDO $db
+     */
+    public function __construct($db)
+    {
+        $this->db = $db;
+        $this->db->sqliteCreateFunction('SCORE', ['PhotoDatabase\Search\FtsFunctions', 'score']);
+    }
+
+    /**
+     * @param $text
+     * @return string
+     */
+    public function prepareQuery($text): string
+    {
+
+        /* TODO
+        if (nounicode) {
+            $text = FtsFunctions::removeDiacritics($text);
+        }
+        */
+        $words = SearchQuery::extractWords($text);
+
+        return SearchQuery::createQuery($words);
+    }
+
+    /**
+     * Full text search returning a list of found keywords in the database.
+     * @param string $text
+     * @return array keywords
+     */
+    public function search(string $text): array
+    {
+        // TODO: non-unicode version
+        $sql = "SELECT * FROM (
+            SELECT ImgId, SUM(SCORE(offsets) * Weight) Rank FROM (
+                SELECT ImgId, OFFSETS(Images_fts) offsets, Weight FROM Images_fts
+                WHERE (Keyword MATCH :text)
+                LIMIT -1 OFFSET 0 -- otherwise will get an error because of subquery flattening
+            )
+            GROUP BY ImgId            
+            ORDER BY Rank DESC)
+            INNER JOIN Images I ON I.Id = ImgId";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':text', $text, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
