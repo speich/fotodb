f802c81299441479de6b34b9d331bd9a7309eff7
Simon <simon.speich@gmail.com>
2021-07-17 13:05:17 +0200

    add missing property and argument types

--- website/scripts/php/class/ExifService.php
+++ website/scripts/php/class/ExifService.php
@@ -24,14 +24,14 @@ class ExifService
 
     private $exiftool;
 
-    private $exiftoolParams = '-c "%+.8f" -g -s2 -j';
+    private string $exiftoolParams = '-c "%+.8f" -g -s2 -j';
 
     /**
      * ExifService constructor.
-     * @param $path
+     * @param string $path
      * @param string $lang
      */
-    public function __construct($path, $lang = 'en')
+    public function __construct(string $path, $lang = 'en')
     {
         $this->exiftool = $path;
         $this->exiftoolParams .= ' -lang '.$lang;    // TODO: check input which is passed as an argument to exiftool
@@ -50,11 +50,12 @@ class ExifService
      * @param string $img full path of image
      * @return array mixed
      */
-    public function getData($img): array
+    public function getData(string $img): array
     {
         $imgInfo = new FileInfoImage($img);
         $extRaw = $imgInfo->getRealPathRaw();
         $extXmp = $imgInfo->getRealPathXmp();
+        $files = false;
 
         $exifService = ExifToolBatch::getInstance($this->exiftool.'/exiftool', $this->exiftoolParams);
         if ($extRaw !== null) {
@@ -63,9 +64,9 @@ class ExifService
         if ($extXmp !== null) {
             $files[] = $extXmp;
         }
-        $exifService->add($files);
+        $exifService?->add($files);
         try {
-            $data = $exifService->fetchAllDecoded(true);
+            $data = $exifService?->fetchAllDecoded(true);
         } catch (ExifToolBatchException $exception) {
             $data = [];
         }
